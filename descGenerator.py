f2630aa315d60d484360c43e7360acf66181c745
david-roper <davidroper321@gmail.com>
2024-05-27 13:53:39 -0400

    adding descMaker function

--- descGenerator.py
+++ descGenerator.py
@@ -1,3 +1,21 @@
+def descMaker(title, desc, default):
+    value_key = title.capitalize()
+    descString = '{ "id": ' + title 
+    + ', \n "name": ' + title + ', \n "description": ' + desc 
+    + ', \n "optional": true, \n "type": "String", \n "value_key": ' + value_key + 
+    ', \n "command-line-flag": --' +  title + ', \n "default-value": ' + default  
+     + '}'
+    return descString
+
 f = open("helpOutput.txt","r")
 
-print(f.read())
+
+
+#print(f.read())
+
+text = f.read()
+
+text = text.split("--")
+text = text[29:]
+
+print(text)
