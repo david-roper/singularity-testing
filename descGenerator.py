b46cc952667d43e5da66af9cd13d46a831ab4586
david-roper <davidroper321@gmail.com>
2024-05-27 15:27:48 -0400

    create boutique generator for preprocess help msg

--- descGenerator.py
+++ descGenerator.py
@@ -1,14 +1,11 @@
 def descMaker(title, desc, default):
-    value_key = title.capitalize()
-    descString = '{ "id": ' + title 
-    + ', \n "name": ' + title + ', \n "description": ' + desc 
-    + ', \n "optional": true, \n "type": "String", \n "value_key": ' + value_key + 
-    ', \n "command-line-flag": --' +  title + ', \n "default-value": ' + default  
-     + '}'
+    value_key = title.upper()
+    descString = '{ "id": ' + title + ', \n "name": ' + title + ', \n "description": ' + desc  + ', \n "optional": true, \n "type": "String", \n "value_key": [' + value_key + '], \n "command-line-flag": --' +  title + ', \n "default-value": ' + default  + '\n}, \n'
     return descString
 
 f = open("helpOutput.txt","r")
-
+open('output.txt', 'w').close()
+outputs = open("output.txt", "a")
 
 
 #print(f.read())
@@ -18,4 +15,29 @@ text = f.read()
 text = text.split("--")
 text = text[29:]
 
-print(text)
+outputs.write("{\n")
+
+for command in text:
+    
+    testText = command.split('\n')
+    firstDesc = testText[0].split("  ")
+    title = firstDesc[0].split(" ")[0]
+    firstDesc = firstDesc[0].split(" ")
+    
+    
+    desc = ' ' +  ' '.join(testText[1:-3])
+    desc = desc.replace("  ", "")
+    
+    defText = ""
+    for defn in testText:
+        if "default:" in defn:
+            defText = "".join(defn)
+            defText = defText.replace("  ", "")
+            defText = defText[defText.index(":") + 1: defText.index(")")]
+        
+
+    description = descMaker(title, desc, defText)
+    outputs.write(description)
+outputs.write('}')
+
+
